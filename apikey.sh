807c56fa2343420234d27eb8ad4a660646b6b957
Robert Oostenveld <r.oostenveld@gmail.com>
2016-01-24 11:43:27 +0100

    source the secret.sh file when present

--- apikey.sh
+++ apikey.sh
@@ -7,3 +7,7 @@ export TWITTER_CONSUMER_SECRET='xxxxxxxxxx'
 export TWITTER_ACCESS_TOKEN_KEY='xxxxxxxxxx'
 export TWITTER_ACCESS_TOKEN_SECRET='xxxxxxxxxx'
 
+if [ -f secret.sh ] ; then
+source secret.sh
+fi
+
