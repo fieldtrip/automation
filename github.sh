acb57a88f31eae9e1062b1d62cdd6c8649b3d884
Robert Oostenveld <r.oostenveld@gmail.com>
2016-01-28 12:26:20 +0100

    added the code to automatically synchronize identical files and to push to bitbucket

--- github.sh
+++ github.sh
@@ -6,3 +6,18 @@ LOGFILE="$FILEPATH"/"$CMDFILE".log
 
 date > $LOGFILE
 
+# get all changes from the master branch on github
+cd $HOME/fieldtrip
+git checkout master
+git pull origin master
+
+# synchronize identical files
+# if there are no changes, this will give "nothing to commit, working directory clean"
+REV=`git log -n 1 --pretty=format:"%H"`
+$HOME/fieldtrip/bin/synchronize-private.sh
+git commit -a -m "automatically synchronized identical files to $REV"
+git push origin master
+
+# also push to bitbucket
+git push bitbucket master
+
