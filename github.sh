11383da27c8581c9b6cbbf775d51327ae648727c
Robert Oostenveld <r.oostenveld@gmail.com>
2017-04-29 12:49:47 +0200

    renamed index.js to service name, makes it easier to keep the different services apart

--- github.sh
+++ github.sh
@@ -3,18 +3,19 @@
 FILEPATH=`dirname "$0"`
 CMDFILE=`basename "$0" .sh`
 LOGFILE="$FILEPATH"/"$CMDFILE".log
+FIELDTRIPDIR=$HOME/fieldtrip/fieldtrip
 
 date > $LOGFILE
 
 # get all changes from the master branch on github
-cd $HOME/fieldtrip
+cd $FIELDTRIPDIR
 git checkout master
 git pull origin master
 
 # synchronize identical files
 # if there are no changes, this will give "nothing to commit, working directory clean"
 REV=`git log -n 1 --pretty=format:"%H"`
-$HOME/fieldtrip/bin/synchronize-private.sh
+$FIELDTRIPDIR/bin/synchronize-private.sh
 git commit -a -m "automatically synchronized identical files to $REV"
 git push origin master
 
