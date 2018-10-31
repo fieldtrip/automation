c665c39e9354bf839e83a2a8b11323fe92dbffa3
Robert Oostenveld <r.oostenveld@gmail.com>
2018-10-31 21:59:33 +0100

    disabled the jekyll process, the website is now built on another server

--- buildsite.sh
+++ buildsite.sh
@@ -3,14 +3,6 @@
 FILEPATH=`dirname "$0"`
 CMDFILE=`basename "$0" .sh`
 LOGFILE="$FILEPATH"/"$CMDFILE".log
-WEBSITEDIR=$HOME/fieldtrip/website
 
-cd $WEBSITEDIR
-date                                                 > $LOGFILE
-echo START                                          >> $LOGFILE
-git pull                                            >> $LOGFILE
-jekyll build                                        >> $LOGFILE
-rsync -arpv --delete _site buildsite@whitepi.local: >> $LOGFILE
-echo DONE                                           >> $LOGFILE
-date                                                >> $LOGFILE
+date > $LOGFILE
 
