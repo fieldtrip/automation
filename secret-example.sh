d1fa9fac8da2dda3486718a62a2857c1a919d2cb
Robert Oostenveld <r.oostenveld@gmail.com>
2019-03-29 10:46:47 +0100

    removed the sourcing of secret.sh, as example it is now also clear

--- secret-example.sh
+++ secret-example.sh
@@ -10,7 +10,3 @@ export TWITTER_CONSUMER_SECRET='xxxxxxxxxx'
 export TWITTER_ACCESS_TOKEN_KEY='xxxxxxxxxx'
 export TWITTER_ACCESS_TOKEN_SECRET='xxxxxxxxxx'
 
-if [ -f secret.sh ] ; then
-source secret.sh
-fi
-
