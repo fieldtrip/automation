6f200583a8d142953548339b3daee141c61b7a83
Robert Oostenveld <r.oostenveld@gmail.com>
2020-08-20 16:51:05 +0200

    implemented a sed.tweet.sh Bash script, this shoudl help with #8

---
+++ send-tweet.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Send an update to twitter whenever something gets pushed to or merged with the fieldtrip repository
+#
+# This makes use of 
+#   - https://github.com/sferik/t
+#   - https://stedolan.github.io/jq/
+#   - https://github.com/jgorset/git.io
+
+URL=`jq .head_commit.url $HOME/.webhook/fieldtrip/payload`
+URL=${URL:1:-1}
+SHORTURL=`git.io $URL`
+
+MESSAGE=`jq .head_commit.message $HOME/.webhook/fieldtrip/payload`
+MESSAGE=${MESSAGE:0:120}
+MESSAGE="$MESSAGE ... See $SHORTURL"
+
+t update "$MESSAGE"
