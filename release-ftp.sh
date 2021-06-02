e5524dd8af7232a4c5c66169ae1a9de0150cd340
Robert Oostenveld <r.oostenveld@gmail.com>
2021-06-02 20:11:57 +0200

    Update release-ftp.sh
    updated target server and directory

--- release-ftp.sh
+++ release-ftp.sh
@@ -69,8 +69,8 @@ else
   cd $TRUNK && git tag $TODAY && git push upstream --tags
 
   # push it to the EEGLAB server
-  $SCP $BASEDIR/daily/fileio-$TODAY.zip         fieldtrip@server.sccn.ucsd.edu:/path/to/incoming/
-  $SCP $BASEDIR/daily/fieldtrip-lite-$TODAY.zip fieldtrip@server.sccn.ucsd.edu:/path/to/incoming/
+  $SCP $BASEDIR/daily/fileio-$TODAY.zip         fieldtrip@sccn.ucsd.edu:upload
+  $SCP $BASEDIR/daily/fieldtrip-lite-$TODAY.zip fieldtrip@sccn.ucsd.edu:upload
 
   # notify Arno that new plugin versions are available for inclusion in EEGLAB
   $CURL "https://sccn.ucsd.edu/eeglab/plugin_uploader/update_donders.php?file=fileio-$TODAY.zip&version=$TODAY&name=Fileio"
