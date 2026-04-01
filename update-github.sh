14c347ee99e2ded27012828a675e75fac0bbd0ee
Robert Oostenveld <r.oostenveld@gmail.com>
2026-04-01 11:41:13 +0200

    removed synchronize identical files, that is now in a GitHub action

--- update-github.sh
+++ update-github.sh
@@ -15,17 +15,10 @@ cd $FIELDTRIPDIR || exit
 git checkout master
 git pull upstream master
 
-# synchronize identical files
-# if there are no changes, this will give "nothing to commit, working directory clean"
-REV=`git log -n 1 --pretty=format:"%H"`
-$FIELDTRIPDIR/bin/synchronize-private.sh
-git commit -a -m "automatically synchronized identical files to $REV"
-git push upstream master
-
-# also push to bitbucket
+# push to bitbucket
 git push bitbucket master
 
-# also push to gitlab
+# push to gitlab
 git push gitlab master
 
 # synchronize the fileio repository
